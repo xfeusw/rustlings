77a7ab1457b5c6a0f441f66320354e80bb41cf67
xfeusw <khamrakulovkamron@gmail.com>
2026-01-12 16:13:00 +0500

    chore: completed option set

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,11 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    match hour_of_day {
+      h if h < 22 => Some(5),
+      h if h < 24 => Some(0),
+      _ => None,
+    }
 }
 
 fn main() {
@@ -18,7 +23,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let ice_creams = maybe_ice_cream(12);
+        let ice_creams = maybe_ice_cream(12).unwrap();
 
         assert_eq!(ice_creams, 5); // Don't change this line.
     }

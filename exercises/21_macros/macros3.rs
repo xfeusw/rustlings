3a887aea6cf9e319b596a9621035c38bdb7f8d50
xfeusw <khamrakulovkamron@gmail.com>
2026-01-16 16:17:15 +0500

    chore: completed macros set

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,13 +1,14 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
 mod macros {
-    macro_rules! my_macro {
-        () => {
-            println!("Check out my macro!");
-        };
-    }
+  #[macro_export]
+  macro_rules! my_macro {
+    () => {
+      println!("Check out my macro!");
+    };
+  }
 }
 
 fn main() {
-    my_macro!();
+  my_macro!();
 }
